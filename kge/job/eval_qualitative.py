505376bef7e21c02a74c647bb084e39bcad5f155
sailera19 <sailer.alex@googlemail.com>
2022-02-18 14:09:49 +0100

    added qualitative eval names, added logging

--- kge/job/eval_qualitative.py
+++ kge/job/eval_qualitative.py
@@ -29,13 +29,26 @@ class QualitativeEvaluationJob(EvaluationJob):
             pin_memory=self.config.get("eval.pin_memory"),
         )
 
+    @torch.no_grad()
     def _evaluate(self):
         chunk_size = self.dataset.num_entities()
         done = False
+
+        # create initial trace entry
+        self.current_trace["epoch"] = dict(
+            type="qualitative_entity_ranking",
+            scope="epoch",
+            split=self.eval_split,
+            epoch=self.epoch,
+            batches=len(self.loader),
+            size=len(self.triples),
+        )
+
         while not done:
             output_list = []
             try:
                 for batch_number, triples in enumerate(self.loader):
+                    self.config.print(f"Starting with Batch Number {batch_number}", end="", flush=True,)
                     triples = triples.to(self.device)
                     batch_scores_sp = None
                     batch_scores_po = None
@@ -43,9 +56,10 @@ class QualitativeEvaluationJob(EvaluationJob):
                         chunk_start = chunk_number * chunk_size
                         chunk_end = (chunk_number + 1) * chunk_size
                         chunk_end = self.dataset.num_entities() if chunk_end > self.dataset.num_entities() else chunk_end
-                        with torch.no_grad():
-                            chunk_scores_sp = self.model.score_sp(triples[:, 0], triples[:, 1], torch.arange(chunk_start, chunk_end, device=self.device))
-                            chunk_scores_po = self.model.score_po(triples[:, 1], triples[:, 2], torch.arange(chunk_start, chunk_end, device=self.device))
+
+                        chunk_scores_sp = self.model.score_sp(triples[:, 0], triples[:, 1], torch.arange(chunk_start, chunk_end, device=self.device))
+                        chunk_scores_po = self.model.score_po(triples[:, 1], triples[:, 2], torch.arange(chunk_start, chunk_end, device=self.device))
+
                         batch_scores_sp = chunk_scores_sp if batch_scores_sp is None else torch.cat((batch_scores_sp, chunk_scores_sp), dim=1)
                         batch_scores_po = chunk_scores_po if batch_scores_po is None else torch.cat(
                             (batch_scores_po, chunk_scores_po), dim=1)
@@ -56,11 +70,11 @@ class QualitativeEvaluationJob(EvaluationJob):
                             batch_scores_po[i].sort(dim=0, descending=True)[1][:self.top_k].tolist()))
                     if len(output_list) >= self.num_samples:
                         break
-
+                self.config.print("\033[2K\r", end="", flush=True)
                 done = True
             except RuntimeError as e:
                 chunk_size = math.ceil(chunk_size / 2)
-                print("new chunk_size: ", chunk_size)
+                self.config.log("Ran out of memory. New chunk_size: ", chunk_size)
 
         file_path = f"{self.config.folder}/quality_eval_{self.eval_split}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
         with open(file_path, "w") as file:
@@ -69,4 +83,22 @@ class QualitativeEvaluationJob(EvaluationJob):
                 file.write(str(item))
                 file.write("\n")
 
-        print("halt")
+        entity_mapping = self.dataset.entity_strings()
+        relation_mapping = self.dataset.relation_strings()
+
+        output_list = [((entity_mapping[x[0][0]], relation_mapping[x[0][1]], entity_mapping[x[0][2]]),
+                        [entity_mapping[y] for y in x[1]],
+                        [entity_mapping[y] for y in x[2]]) for x in output_list]
+
+        file_path = f"{self.config.folder}/quality_eval_names_{self.eval_split}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+        with open(file_path, "w") as file:
+
+            for item in output_list:
+                file.write(str(item))
+                file.write("\n")
+
+        self.config.print("Quality Evaluation finished")
+        for output in output_list:
+            self.config.print(f"Tail for triple {output[0]}: {output[1]}")
+            self.config.print(f"Head for triple {output[0]}: {output[2]}")
+
